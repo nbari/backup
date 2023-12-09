7155ca35e1054cbe1a0dafbbfd20425d2b39f275
nbari <nbari@tequila.io>
2023-12-09 23:37:24 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use clap::{App, Arg};
-
 fn main() {
-    let matches = App::new("backup")
-        .version(env!("CARGO_PKG_VERSION"))
-        .arg(
-            Arg::with_name("quiet")
-                .required(false)
-                .takes_value(false)
-                .long("quiet")
-                .short("q")
-                .help("suppress output messages"),
-        )
-        .get_matches();
+    todo!();
 }
